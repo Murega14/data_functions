4e7fcf1e5be1ce8a919a3fe528f7074b18987e60
Murega14 <tedmurega@gmail.com>
2023-11-20 14:14:10 +0300

    added more functions

--- data_func.py
+++ data_func.py
@@ -2,6 +2,122 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
+import chardet
+
+def load_data(file_path, encoding=None, file_types=None):
+    """
+    Loads data from a file into a pandas dataframe
+    It determines the file type and encoding automatically
+
+    Args:
+        file_path (str): The path to the file being loaded
+        encoding (str, optional): The encoding of the file being loaded. Defaults to None
+        file_types: (list, optional): List of accepted file extensions. Defaults to None
+            ['.csv', '.xls', '.xlsx', '.json', '.html', '.xml', '.clipboard', '.excel',
+            '.hdf', '.feather', '.parquet', '.orc', '.stata', '.sas', '.spss', '.pickle', '.sql', '.gbq'].
+
+    Returns:
+        pandas.Dataframe: The loaded dataframe
+
+    Raises:
+        ValueError: If the file type is not supported or the encoding is not valid
+
+    Examples:
+        # Load a csv file
+        >>> df = load_data('data.csv')
+
+        # Load a xlsx file
+        >>> df = load_data('data.xlsx')
+
+        # Load a xls file
+        >>> df = load_data('data.xls')
+
+        # Load a json file
+        >>> df = load_data('data.json')
+
+        # Load a html file
+        >>> df = load_data('data.html')
+
+        # Load a xml file
+        >>> df = load_data('data.xml')
+
+        # Load a clipboard file
+        >>> df = load_data('data.clipboard')
+
+        # Load a excel file
+        >>> df = load_data('data.excel')
+
+        # Load a hdf file
+        >>> df = load_data('data.hdf')
+
+        # Load a feather file
+        >>> df = load_data('data.feather')
+
+        # Load a parquet file
+        >>> df = load_data('data.parquet')
+    """
+    if file_types is None:
+        file_types = ['.csv', '.xls', '.xlsx', '.json', '.html', '.xml', '.clipboard', '.excel',
+                      '.hdf', '.feather', '.parquet', '.orc', '.stata', '.sas', '.spss', '.pickle', '.sql', '.gbq']
+
+    if encoding is None:
+        encoding = 'utf-8'
+
+    try:
+        with open(file_path, 'rb') as f:
+            rows = f.read()
+            encoding = chardet.detect(rows).get('encoding')
+
+        if any(file_path.endswith(file_type) for file_type in file_types):
+            if file_path.endswith('.csv'):
+                df = pd.read_csv(file_path, encoding=encoding)
+            elif file_path.endswith('.xls') or file_path.endswith('.xlsx'):
+                df = pd.read_excel(file_path, encoding=encoding)
+            elif file_path.endswith('.json'):
+                df = pd.read_json(file_path, encoding=encoding)
+            elif file_path.endswith('.html'):
+                df = pd.read_html(file_path, encoding=encoding)
+            elif file_path.endswith('.xml'):
+                df = pd.read_xml(file_path, encoding=encoding)
+            elif file_path.endswith('.clipboard'):
+                df = pd.read_clipboard(encoding=encoding)
+            elif file_path.endswith('.excel'):
+                df = pd.read_excel(file_path, encoding=encoding)
+            elif file_path.endswith('.hdf'):
+                df = pd.read_hdf(file_path, encoding=encoding)
+            elif file_path.endswith('.feather'):
+                df = pd.read_feather(file_path, encoding=encoding)
+            elif file_path.endswith('.parquet'):
+                df = pd.read_parquet(file_path, encoding=encoding)
+            elif file_path.endswith('.orc'):
+                df = pd.read_orc(file_path, encoding=encoding)
+            elif file_path.endswith('.stata'):
+                df = pd.read_stata(file_path, encoding=encoding)
+            elif file_path.endswith('.sas'):
+                df = pd.read_sas(file_path, encoding=encoding)
+            elif file_path.endswith('.spss'):
+                df = pd.read_spss(file_path, encoding=encoding)
+            elif file_path.endswith('.pickle'):
+                df = pd.read_pickle(file_path, encoding=encoding)
+            elif file_path.endswith('.sql'):
+                df = pd.read_sql(file_path, encoding=encoding)
+            elif file_path.endswith('.gbq'):
+                df = pd.read_gbq(file_path, encoding=encoding)
+        else:
+            raise ValueError(f"File type {os.path.splitext(file_path)[1]} is not supported.")
+    
+    except Exception as e:
+        raise ValueError(f"Error loading file: {e}")
+    # Check if the file extension is in the file_types list
+    if os.path.splitext(file_path)[1] not in file_types:
+        raise ValueError(f"File type {os.path.splitext(file_path)[1]} is not supported.")
+
+    # Load the data based on the file extension
+    if os.path.splitext(file_path)[1] == '.csv':
+        df = pd.read_csv(file_path, encoding=encoding)
+    elif os.path.splitext(file_path)[1] == '.xls':
+        df = pd.read_excel(file_path, encoding=encoding)
 
 
 def check_duplicates(df):
